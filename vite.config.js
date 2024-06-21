2637150fc69fde2f3cf5708f1c014d0697e18e36
Ashish Goyal <ashish.goyal197@gmail.com>
2024-06-21 16:24:02 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  base: "/GoFood/",
+  // server: {
+  //   port: 3000,
+  // },
 });
