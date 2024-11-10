7bb468068d20454a4b16f03b272e506c429a9617
Ashish Goyal <ashish.goyal197@gmail.com>
2024-11-10 22:08:07 +0530

    deployment process

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,15 @@ mongoDB();
 dotenv.config();
 
 // deployement
-const ___dirname = path.resolve();
+// const ___dirname = path.resolve();
 
 //
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", [
+    "http://localhost:3000",
+    "https://gofood-brzj.onrender.com",
+  ]);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -36,10 +39,10 @@ app.use("/api", DisplayData);
 app.use("/api", OrderData);
 
 // deployment
-app.use(express.static(path.join(___dirname, "/frontend/dist")));
+app.use(express.static(path.join(path.resolve(), "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(___dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(path.resolve(), "frontend", "dist", "index.html"));
 });
 
 //
